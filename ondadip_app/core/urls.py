a600d41dad5f427102a9cdb2c52ca6f77fdc63b8
Jason Jea <jasonzjea@gmail.com>
2020-04-05 00:09:31 -0700

    create an add security functionality; need to turn multiple choice form into dropdown form.  need to create remove security option

--- ondadip_app/core/urls.py
+++ ondadip_app/core/urls.py
@@ -5,8 +5,10 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('user/<int:user_id>/', views.user, name='user'),
-    path('security/<int:security_id>/', views.security, name='security'),
-    path('user/<int:user_id>/portfolio/', views.portfolio, name='portfolio'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('user/<int:pk>/', views.UserView.as_view(), name='user'),
+    path('security/<int:pk>/', views.SecurityView.as_view(), name='security'),
+    path('user/<int:pk>/portfolio/', views.PortfolioView.as_view(), name='portfolio'),
+    path('<int:user_id>/vote/', views.security_add, name='security_add'),
+    path('<int:pk>/security_added/', views.SecurityAddedView.as_view(), name='security_added')
 ]
--- ondadip_app/core/views.py
+++ ondadip_app/core/views.py
@@ -1,36 +1,70 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from numpy import setdiff1d
 from .models import Security, User, UserPortfolio
 
 
-def index(request):
-    latest_securities_list = Security.objects.order_by('-created_at')[:5]
+class IndexView(generic.ListView):
+    template_name = 'core/index.html'
+    context_object_name = 'latest_securities_list'
 
-    context = {
-        'latest_securities_list': latest_securities_list,
-    }
-
-    return render(request, 'core/index.html', context)
-
-
-def user(request, user_id):
-    user = User.objects.get(pk=user_id)
-
-    return render(request, 'core/user.html', {'user': user})
+    def get_queryset(self):
+        """Return the last five created securities"""
+        return Security.objects.order_by('-created_at')[:5]
 
 
-def security(request, security_id):
-    security = get_object_or_404(Security, pk=security_id)
+class UserView(generic.DetailView):
+    model = User
+    template_name = 'core/user.html'
 
-    return render(request, 'core/security.html', {'security': security})
+
+class SecurityView(generic.DetailView):
+    template_name = 'core/security.html'
+    model = Security
 
 
-def portfolio(request, user_id):
-    user = User.objects.get(pk=user_id)
-    portfolio_securities_list = user.portfolio_security.all()
-    context = {
-        'portfolio_securities_list': portfolio_securities_list,
-        'user': user,
-    }
-    return render(request, 'core/portfolio.html', context)
+class PortfolioView(generic.DetailView):
+    template_name = 'core/portfolio.html'
+    model = User
+
+    def get_context_data(self, **kwargs):
+        """Overriden to add `portfolio` to the template context."""
+        context = super().get_context_data(**kwargs)
+        all_securities = Security.objects.all()
+        existing_securities = context['user'].portfolio_security.all()
+
+        securities_symbols_final = setdiff1d(
+            [e.symbol for e in all_securities],
+            [e.security.symbol for e in existing_securities],
+            assume_unique=False
+        ).tolist()
+
+        securities = Security.objects.filter(symbol__in=securities_symbols_final)
+
+        context['securities'] = securities
+        return context
+
+
+class SecurityAddedView(generic.DetailView):
+    model = User
+    template_name = 'core/security_added.html'
+
+
+def security_add(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    try:
+        selected_security = Security.objects.get(pk=int(request.POST['security']))
+    except (KeyError, UserPortfolio.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'core/portfolio.html', {
+            'error_message': "You didn't select a security.",
+        })
+    else:
+        user.add_to_portfolio(selected_security)
+        print("Added " + selected_security.symbol)
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('core:security_added', args=(user.id,)))
