20f5923ffa0f092e1aa182a5d7ecc98ba46947ef
Jason Jea <jasonzjea@gmail.com>
2020-04-05 00:47:50 -0700

    comments and clean up urls

--- ondadip_app/core/urls.py
+++ ondadip_app/core/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('user/<int:pk>/', views.UserView.as_view(), name='user'),
     path('security/<int:pk>/', views.SecurityView.as_view(), name='security'),
     path('user/<int:pk>/portfolio/', views.PortfolioView.as_view(), name='portfolio'),
-    path('<int:user_id>/vote/', views.security_add, name='security_add'),
-    path('<int:pk>/security_added/', views.SecurityAddedView.as_view(), name='security_added')
+    path('user/<int:user_id>/portfolio/add/', views.security_add, name='security_add'),
+    path('user/<int:pk>/portfolio/security_added/', views.SecurityAddedView.as_view(), name='security_added')
 ]
--- ondadip_app/core/views.py
+++ ondadip_app/core/views.py
@@ -7,6 +7,7 @@ from .models import Security, User, UserPortfolio
 
 
 class IndexView(generic.ListView):
+    """homepage lists latest 5 securities by created date"""
     template_name = 'core/index.html'
     context_object_name = 'latest_securities_list'
 
@@ -16,25 +17,31 @@ class IndexView(generic.ListView):
 
 
 class UserView(generic.DetailView):
+    # subclassing Django's generic detail view (returns details of a model object)
+    # this is a view that returns details about a user
     model = User
     template_name = 'core/user.html'
 
 
 class SecurityView(generic.DetailView):
+    """returns details about a security"""
     template_name = 'core/security.html'
     model = Security
 
 
 class PortfolioView(generic.DetailView):
+    """returns details about a user's portfolio"""
     template_name = 'core/portfolio.html'
     model = User
 
     def get_context_data(self, **kwargs):
-        """Overriden to add `portfolio` to the template context."""
-        context = super().get_context_data(**kwargs)
+        """Override default to add a list of available securities for the user to the template context."""
+        context = super().get_context_data(**kwargs) # this grabs the existing context for this view (just the user object)
         all_securities = Security.objects.all()
         existing_securities = context['user'].portfolio_security.all()
 
+        # using set logic, return items from set1 that don't exist in set2
+        # this is the same as a left join filtering where left_table.id is null
         securities_symbols_final = setdiff1d(
             [e.symbol for e in all_securities],
             [e.security.symbol for e in existing_securities],
@@ -48,17 +55,23 @@ class PortfolioView(generic.DetailView):
 
 
 class SecurityAddedView(generic.DetailView):
+    """Results page after adding a security."""
     model = User
     template_name = 'core/security_added.html'
 
 
 def security_add(request, user_id):
+    """Method for adding a security through a form to a user's portfolio."""
+    # this is the manual way of retrieving the relevant object through a GET request
     user = get_object_or_404(User, pk=user_id)
     try:
+        # when user hits 'add' button, request.POST returns a dictionary
+        # in this case just one entry 'security' (security.id to be more specific)
         selected_security = Security.objects.get(pk=int(request.POST['security']))
     except (KeyError, UserPortfolio.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'core/portfolio.html', {
+            'user': user,
             'error_message': "You didn't select a security.",
         })
     else:
